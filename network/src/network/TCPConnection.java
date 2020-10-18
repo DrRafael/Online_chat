32b9c186cdc7ac0abddb55b1a3e7dd54c671723e
Rafael <rfl777@mail.ru>
2020-10-18 20:43:48 -0300

    ready class network

--- network/src/network/TCPConnection.java
+++ network/src/network/TCPConnection.java
@@ -10,10 +10,17 @@ public class TCPConnection {
 
     private final Socket socket;
     private final Thread rxThead;
+    private  final TCPConnectionListenner eventListEner;
     private final BufferedReader in;
     private final BufferedWriter out;
 
-    public TCPConnection(Socket socket) throws IOException {
+    public TCPConnection(TCPConnectionListenner eventListEner, String ipAddr, int port) throws IOException{
+        this(eventListEner,new Socket(ipAddr, port));
+    }
+
+
+    public TCPConnection(TCPConnectionListenner eventListEner, Socket socket) throws IOException {
+        this.eventListEner = eventListEner;
         this.socket = socket;
         in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
         out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
@@ -21,15 +28,44 @@ public class TCPConnection {
             @Override
             public void run() {
                 try{
+                    eventListEner.onConnectionReady(TCPConnection.this);
+                    while (!rxThead.isInterrupted()){
+                        eventListEner.onReceiveString(TCPConnection.this,in.readLine());
+                    }
                String msg = in.readLine();
             } catch (IOException e){
-
+                    eventListEner.onException(TCPConnection.this, e);
                 }finally {
-
+                   eventListEner.onDisconnect(TCPConnection.this);
                 }
             }
         });
         rxThead.start();
+    }
 
+    public synchronized void sendstring(String value){
+        try {
+            out.write(value + "\r\n");
+            out.flush();
+        } catch (IOException e) {
+            eventListEner.onException(TCPConnection.this, e);
+            disconect();
+        }
+
+    }
+
+    public synchronized void disconect(){
+        rxThead.interrupt();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            eventListEner.onException(TCPConnection.this, e);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "TCPConnection: " + socket.getInetAddress() + ": " + socket.getPort();
     }
 }
